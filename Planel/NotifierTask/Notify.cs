b3961fa028f4be1a55aaf760b97b3260881e118e
NGame <NGame@DESKTOP-U001LC2>
2017-01-09 22:04:06 +0330

    Base changes for background task

---
+++ Planel/NotifierTask/Notify.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+
+namespace NotifierTask
+{
+    public sealed class Notify : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+
+        }
+    }
+}
