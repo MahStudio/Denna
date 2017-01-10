7c4c59de90f16274f87a39daed2bb9440182cd09
@Mohsens22 <mohsens2@outlook.com>
2017-01-10 21:57:47 +0330

    added background live tile refresher

--- Planel/NotifierTask/Notify.cs
+++ Planel/NotifierTask/Notify.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
+using Windows.Data.Xml.Dom;
+using Windows.Storage;
 using Windows.UI.Notifications;
 
 namespace NotifierTask
@@ -17,6 +20,8 @@ namespace NotifierTask
             taskInstance.Canceled += TaskInstance_Canceled;
             taskInstance.Task.Completed += Task_Completed;
             NotifyCheck();
+            livetile();
+            updatebadge();
         }
         void SendToastNotification(string Content, string Parameters, bool IsDurationLong = false)
         {
@@ -51,6 +56,58 @@ namespace NotifierTask
             {
                 SendToastNotification(item.title + " : " + item.detail, item.Id.ToString(), true);
             }
+        }
+        public static async void livetile()
+        {
+            ObservableCollection<todo> todolist = new ObservableCollection<todo>();
+            string result = " Dont forget to :  ";
+            DateTime now = DateTime.Now;
+            todolist = Localdb.Getfordoday(now);
+            foreach (var item in todolist)
+            {
+                if (item.isdone != 2)
+                    result += "," + item.title + "  ";
+
+            }
+            result.Replace("\n", Environment.NewLine);
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(await FileIO.ReadTextAsync(await StorageFile.GetFileFromApplicationUriAsync(
+                new Uri("ms-appx:///LiveTile.xml"))));
+            //Set Medium tile
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile sampleFile = await storageFolder.GetFileAsync("avatar.jpg");
+            xmlDoc.LoadXml(xmlDoc.GetXml().Replace("TileMediumImageSource", sampleFile.Path));
+            xmlDoc.LoadXml(xmlDoc.GetXml().Replace("TileMediumtext", "Dear " + ApplicationData.Current.LocalSettings.Values["Username"].ToString()));
+            xmlDoc.LoadXml(xmlDoc.GetXml().Replace("TileMediumSubText", "Let's Do!"));
+            //Set Wide Tile 
+            xmlDoc.LoadXml(xmlDoc.GetXml().Replace("TileWideImageSource", sampleFile.Path));
+            xmlDoc.LoadXml(xmlDoc.GetXml().Replace("TileWideText", result));
+
+
+
+            var tup = TileUpdateManager.CreateTileUpdaterForApplication();
+            tup.Update(new TileNotification(xmlDoc));
+
+
+
+
+
+
+        }
+        public static async void updatebadge()
+        {
+            var type = BadgeTemplateType.BadgeNumber;
+            var xml = BadgeUpdateManager.GetTemplateContent(type);
+
+            var elements = xml.GetElementsByTagName("badge");
+            var element = elements[0] as Windows.Data.Xml.Dom.XmlElement;
+            int val = Localdb.counter();
+            element.SetAttribute("value", val.ToString());
+
+            var updator = BadgeUpdateManager.CreateBadgeUpdaterForApplication();
+            var notification = new BadgeNotification(xml);
+            updator.Update(notification);
         }
 
         private void Task_Completed(BackgroundTaskRegistration sender, BackgroundTaskCompletedEventArgs args)
