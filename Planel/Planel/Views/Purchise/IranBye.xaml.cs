1fc33a4290106bbf598c48ee4b86c29e4d1f3f59
@Mohsens22 <mohsens2@outlook.com>
2017-01-25 16:27:22 +0330

    added in app payment for IR licence

--- Planel/Planel/Views/Purchise/IranBye.xaml.cs
+++ Planel/Planel/Views/Purchise/IranBye.xaml.cs
@@ -12,6 +12,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Planel.ZarinPal;
+using Windows.Storage;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +24,65 @@ namespace Planel.Views
     /// </summary>
     public sealed partial class IranBye : Page
     {
+        string merchantCode = "3820a6ee-e2ab-11e6-8aa9-000c295eb8fc";
+        string authority = string.Empty;
+        int amount = 100;
+        string description = "Denna licence IR";
+        string email = string.Empty;
+        string mobile = string.Empty;
+        string callBackUrl = "http://mahholding.ir/payment/";
         public IranBye()
         {
             this.InitializeComponent();
         }
+
+        private async void goit_Click(object sender, RoutedEventArgs e)
+        {
+            mobile = mob.Text;
+            email = eemail.Text;
+
+            if (amount < 100 || string.IsNullOrEmpty(merchantCode) || string.IsNullOrEmpty(description) || string.IsNullOrEmpty(callBackUrl))
+                return;
+
+            PaymentGatewayImplementationServicePortTypeClient zp = new PaymentGatewayImplementationServicePortTypeClient();
+
+            PaymentRequestResponse response = await zp.PaymentRequestAsync(merchantCode, amount, description, email, mobile, callBackUrl);
+
+            authority = response.Body.Authority;
+
+            if (response.Body.Status == 100)
+            {
+                webView.Visibility = Visibility.Visible;
+                webView.Navigate(new Uri("https://www.zarinpal.com/pg/StartPay/" + authority));
+            }
+            else
+                ShowMessage("There is a problem in creating authority code. please contact developer \r\nStatus code: " + response.Body.Status);
+        }
+        
+        async private void webView_NavigationCompleted(WebView sender, WebViewNavigationCompletedEventArgs args)
+        {
+            // آدرس فعلی ای که وب ویو در آن است
+            string myUrl = sender.Source.ToString();
+
+            if (myUrl.Contains(callBackUrl))
+            {
+                sender.Visibility = Visibility.Collapsed;
+
+                PaymentGatewayImplementationServicePortTypeClient zp = new PaymentGatewayImplementationServicePortTypeClient();
+
+                PaymentVerificationResponse response = await zp.PaymentVerificationAsync(merchantCode, authority, amount);
+
+                if (response.Body.Status == 100)
+                    ShowMessage("Purchase succeed.\r\n please restart the app due to licence activation \r\n RefId in case you need: " + response.Body.RefID);
+                else
+                    ShowMessage("There is a problem while purchasing... please contact developer \r\nStatus code: " + response.Body.Status);
+                ApplicationData.Current.LocalSettings.Values["LicenceActive"] = true;
+            }
+        }
+        async void ShowMessage(string content)
+        {
+            await new Windows.UI.Popups.MessageDialog(content).ShowAsync();
+        }
+
     }
 }
