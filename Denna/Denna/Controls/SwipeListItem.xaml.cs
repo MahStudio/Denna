4397a8c355fbb1de34abde49c77c36fda49d05b5
@Mohsens22 <mohsens2@outlook.com>
2017-04-21 13:08:01 +0430

    Partial swipe list view

---
+++ Denna/Denna/Controls/SwipeListItem.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Denna.Controls
+{
+    public sealed partial class SwipeListItem : UserControl
+    {
+        Point startpoint;
+        Point lastPostition;
+        public SwipeListItem()
+        {
+            this.InitializeComponent();
+        }
+
+        private void MainGrid_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            if (myScaleTransform.X >= 60 && e.Delta.Translation.X >1) return;
+            if (myScaleTransform.X <= -180 && e.Delta.Translation.X < 1) return;
+            myScaleTransform.X += e.Delta.Translation.X;
+            lastPostition.X += e.Delta.Translation.X;
+           
+        }
+
+        private void MainGrid_ManipulationCompleted(object sender, ManipulationCompletedRoutedEventArgs e)
+        {
+           
+        }
+
+        private void MainGrid_ManipulationStarted(object sender, ManipulationStartedRoutedEventArgs e)
+        {
+            startpoint = e.Position;
+            lastPostition = e.Position;
+        }
+    }
+}
