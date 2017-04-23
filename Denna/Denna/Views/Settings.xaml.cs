10ffe13047d1893fd29d34f949b864d95b2fbd65
@Mohsens22 <mohsens2@outlook.com>
2017-04-23 09:26:27 +0430

    Added navigation

--- Denna/Denna/Views/Settings.xaml.cs
+++ Denna/Denna/Views/Settings.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Denna.Views.SubSettings;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -29,7 +30,52 @@ namespace Denna.Views
 
         private void ArtistsList_ItemClick(object sender, ItemClickEventArgs e)
         {
-            
+            var clk = e.ClickedItem as Classes.ItemHolder;
+            switch (clk.ID)
+            {
+                case 1 :
+                    {
+                        Frame.Navigate(typeof(Account));
+                        break;
+                    }
+                case 2:
+                    {
+                        Frame.Navigate(typeof(Privacy));
+                        break;
+                    }
+                case 3:
+                    {
+                        Frame.Navigate(typeof(Notifications));
+                        break;
+                    }
+                case 4:
+                    {
+                        Frame.Navigate(typeof(QuickActions));
+                        break;
+                    }
+                case 5:
+                    {
+                        Frame.Navigate(typeof(Personalization));
+                        break;
+                    }
+                case 6:
+                    {
+                        Frame.Navigate(typeof(Language));
+                        break;
+                    }
+                case 7:
+                    {
+                        Frame.Navigate(typeof(Support));
+                        break;
+                    }
+                case 8:
+                    {
+                        Frame.Navigate(typeof(About));
+                        break;
+                    }
+                default:
+                    break;
+            }
         }
     }
 }
