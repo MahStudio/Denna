bcd6a0c339fefad33c0a2e714f3a00bcb2f0e46e
@Mohsens22 <mohsens2@outlook.com>
2017-04-25 03:45:41 +0430

    Added a base extended splash screen

--- Denna/Denna/App.xaml.cs
+++ Denna/Denna/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Denna.Views;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -66,49 +67,17 @@ namespace Denna
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
         /// <param name="e">Details about the launch request and process.</param>
-        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
         {
             stuff();
-            Frame rootFrame = Window.Current.Content as Frame;
-
-            // Do not repeat app initialization when the Window already has content,
-            // just ensure that the window is active
-            if (rootFrame == null)
+            if (args.PreviousExecutionState != ApplicationExecutionState.Running)
             {
-                // Create a Frame to act as the navigation context and navigate to the first page
-                rootFrame = new Frame();
-
-                rootFrame.NavigationFailed += OnNavigationFailed;
-
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
-                // Place the frame in the current Window
-                Window.Current.Content = rootFrame;
+                bool loadState = (args.PreviousExecutionState == ApplicationExecutionState.Terminated);
+                ExtendedSplash extendedSplash = new ExtendedSplash(args.SplashScreen, loadState);
+                Window.Current.Content = extendedSplash;
             }
 
-            if (e.PrelaunchActivated == false)
-            {
-                if (rootFrame.Content == null)
-                {
-                    // When the navigation stack isn't restored navigate to the first page,
-                    // configuring the new page by passing required information as a navigation
-                    // parameter
-                    if (ApplicationData.Current.LocalSettings.Values["Firstrun"] as string == "1")
-                    {
-                        rootFrame.Navigate(typeof(Views.PageMaster), e.Arguments);
-                        
-                    }
-
-                    else
-                        rootFrame.Navigate(typeof(Views.PageMaster), e.Arguments);
-
-                }
-                // Ensure the current window is active
-                Window.Current.Activate();
-            }
+            Window.Current.Activate();
         }
 
         /// <summary>
---
+++ Denna/Denna/Views/ExtendedSplash.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Denna.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class ExtendedSplash : Page
+    {
+        internal Rect splashImageRect; // Rect to store splash screen image coordinates.
+        private SplashScreen splash; // Variable to hold the splash screen object.
+        internal bool dismissed = false; // Variable to track splash screen dismissal status.
+        internal Frame rootFrame;
+
+        public ExtendedSplash(SplashScreen splashscreen, bool loadState)
+        {
+            InitializeComponent();
+
+            // Listen for window resize events to reposition the extended splash screen image accordingly.
+            // This ensures that the extended splash screen formats properly in response to window resizing.
+            Window.Current.SizeChanged += new WindowSizeChangedEventHandler(ExtendedSplash_OnResize);
+
+            splash = splashscreen;
+            if (splash != null)
+            {
+                // Register an event handler to be executed when the splash screen has been dismissed.
+                splash.Dismissed += new TypedEventHandler<SplashScreen, Object>(DismissedEventHandler);
+
+                // Retrieve the window coordinates of the splash screen image.
+                splashImageRect = splash.ImageLocation;
+                
+            }
+
+            // Create a Frame to act as the navigation context
+            rootFrame = new Frame();
+            
+        }
+
+        private void ExtendedSplash_OnResize(object sender, WindowSizeChangedEventArgs e)
+        {
+           // throw new NotImplementedException();
+        }
+
+
+        // Include code to be executed when the system has transitioned from the splash screen to the extended splash screen (application's first view).
+        void DismissedEventHandler(SplashScreen sender, object e)
+        {
+            dismissed = true;
+
+            // Complete app setup operations here...
+
+           
+        }
+        void DismissExtendedSplash()
+        {
+            // Navigate to mainpage
+            rootFrame.Navigate(typeof(PageMaster));
+            // Place the frame in the current Window
+            Window.Current.Content = rootFrame;
+        }
+        
+        async void RestoreStateAsync(bool loadState)
+        {
+            if (loadState)
+            {
+                // code to load your app's state here
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DismissExtendedSplash();
+        }
+    }
+}
