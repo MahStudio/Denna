c94a62089917f0323768e48a6878622829db75c0
@Mohsens22 <mohsens2@outlook.com>
2017-05-10 16:25:17 +0430

    Couchbase.Lite is inaccessable due to internal error on CB servers

--- Denna/Core/Models/TasksModel.cs
+++ Denna/Core/Models/TasksModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Couchbase.Lite;
+//using Couchbase.Lite;
 using System.IO;
 
 namespace Core.Models
@@ -12,22 +12,22 @@ namespace Core.Models
     {
         public void createDB()
         {
-            Couchbase.Lite.Support.UWP.Activate();
-            DatabaseOptions options = new DatabaseOptions();
-            options.EncryptionKey = EncryptionKeyFactory.Create("PassHash");
-            var database = DatabaseFactory.Create("Xugros",options);
-            var doc = database.CreateDocument();
-            doc.Properties = new Dictionary<string, object>
-            {
-                ["type"] = "user",
-                ["admin"] = false,
-                ["address"] = new Dictionary<string, object>
-                {
-                    ["street"] = "1 park street",
-                    ["zip"] = 123456
-                }
-            };
-            doc.Save();
+        //    Couchbase.Lite.Support.UWP.Activate();
+        //    DatabaseOptions options = new DatabaseOptions();
+        //    options.EncryptionKey = EncryptionKeyFactory.Create("PassHash");
+        //    var database = DatabaseFactory.Create("Xugros",options);
+        //    var doc = database.CreateDocument();
+        //    doc.Properties = new Dictionary<string, object>
+        //    {
+        //        ["type"] = "user",
+        //        ["admin"] = false,
+        //        ["address"] = new Dictionary<string, object>
+        //        {
+        //            ["street"] = "1 park street",
+        //            ["zip"] = 123456
+        //        }
+        //    };
+        //    doc.Save();
 
 
         }
