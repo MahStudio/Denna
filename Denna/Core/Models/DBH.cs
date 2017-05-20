a2466f4268c4c141bec1cbfc3b1f53bc497cf786
@Mohsens22 <mohsens2@outlook.com>
2017-05-20 04:40:24 +0430

    About page changes

--- Denna/Core/Models/DBH.cs
+++ Denna/Core/Models/DBH.cs
@@ -10,43 +10,42 @@ namespace Core.Models
 {
     public static class DBH
     {
-        static IDatabase _database;
+        
         public static void CreateDB()
         {
             Couchbase.Lite.Support.UWP.Activate();
             DatabaseOptions options = new DatabaseOptions();
-            options.EncryptionKey = EncryptionKeyFactory.Create("PassHash");
             Windows.Storage.StorageFolder storageFolder =
             Windows.Storage.ApplicationData.Current.LocalFolder;
+            var database = new Database("Xugros", options);
             options.Directory = storageFolder.Path;
-            var database = DatabaseFactory.Create("Xugros", options);
+
             
-            _database = database;
             
         }
 
         public static void MakeDoc(Dictionary<string,object> Dic)
         {
             Couchbase.Lite.Support.UWP.Activate();
-            var database = DatabaseFactory.Create("Xugros");
-            var doc = database.CreateDocument();
-            doc.Properties = Dic;
-            doc.Save();
+            var database = new Database("Xugros");
+           // var doc = database.CreateDocument();
+            //doc.Properties = Dic;
+            //doc.Save();
 
         }
         public static string Query()
         {
-            Couchbase.Lite.Support.UWP.Activate();
-            var database = DatabaseFactory.Create("Xugros");
+            //    Couchbase.Lite.Support.UWP.Activate();
+            //    var database = DatabaseFactory.Create("Xugros");
 
-            var query = QueryFactory.Select()
-        .From(DataSourceFactory.Database(database))
-        .Where(
-            ExpressionFactory.Property("Type").EqualTo("user")
-        );
+            //    var query = QueryFactory.Select()
+            //.From(DataSourceFactory.Database(database))
+            //.Where(
+            //    ExpressionFactory.Property("Type").EqualTo("user")
+            //);
 
-            var rows = query.Run();
-            return rows.FirstOrDefault().Document.Id;
+            //    var rows = query.Run();
+            return "";
         }
 
         
