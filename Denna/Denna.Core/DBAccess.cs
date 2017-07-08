9cd384575bf33075e2d2a8da799bee75ad7f7127
@Mohsens22 <mohsens2@outlook.com>
2017-07-08 13:30:45 +0430

    More work on New Core

--- Denna/Denna.Core/DBAccess.cs
+++ Denna/Denna.Core/DBAccess.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Couchbase.Lite;
 
 namespace Denna.Core
 {
     internal class DBAccess
     {
+        internal void CreateDB()
+        {
+            var database = new Database("Xugros");
+            DB = database;
 
+        }
     }
 }
