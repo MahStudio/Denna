a24d4604a9b4f8fa3c74d089dee8906fa7a92e2b
Mohsen <mohsens2@outlook.com>
2018-07-27 23:49:35 +0430

    app center shit

--- Denna/appcenter-pre-build.ps1
+++ Denna/appcenter-pre-build.ps1
@@ -1,1 +1,7 @@
 Add-Content %APPCENTER_SOURCE_DIRECTORY%\Denna\Core\Constants.cs %DennaCode%
+
+Out-Host Initializing
+
+Out-Host %DennaCode%
+
+Out-Host %APPCENTER_SOURCE_DIRECTORY%\Denna\Core\Constants.cs
