2fa205f068f97c3299ad61cf7acbbcdb67c9d5ba
Mohsen <mohsens2@outlook.com>
2018-07-29 02:51:14 +0430

    fixed #2

---
+++ Denna/Denna/Converters/IntToVisibilityConverter.cs
@@ -0,0 +1,29 @@
+﻿using Core.Domain;
+using Realms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Denna.Converters
+{
+    public class IntToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var count = (int)value;
+            if (count == 0)
+                return Visibility.Visible;
+            else
+                return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
