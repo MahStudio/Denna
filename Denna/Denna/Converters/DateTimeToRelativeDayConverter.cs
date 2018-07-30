3190c01cef501d6d0ad7e27e745f62f62df1d093
Mohsen <mohsens2@outlook.com>
2018-07-30 20:32:02 +0430

    fix #12

---
+++ Denna/Denna/Converters/DateTimeToRelativeDayConverter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Denna.Converters
+{
+    public class DateTimeToRelativeDayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            DateTimeOffset Value = (DateTimeOffset)value;
+            var Day = Value.Date;
+            string x = "";
+            if (Day == DateTime.Today)
+                x += "Today";
+            else if (Day == DateTime.Today.AddDays(1))
+                x += "Tomorrow";
+            else if (Day == DateTime.Today.AddDays(-1))
+                x += "Yesterday";
+            else
+            {
+                var month = Value.Month;
+                var day = Value.Day;
+                x += month + "/" + day;
+            }
+            string Hour = Value.Hour.ToString();
+            string Min = Value.Minute.ToString();
+            
+            x += " " + Hour + ":" + Min;
+
+
+            return x;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
