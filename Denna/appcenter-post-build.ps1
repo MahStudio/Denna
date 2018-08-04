52241b678be29d5343b5fbc2e6501ea961278861
Mohsen <mohsens2@outlook.com>
2018-08-04 13:27:30 +0430

    changed postbuild

--- Denna/appcenter-post-build.ps1
+++ Denna/appcenter-post-build.ps1
@@ -1,3 +1,12 @@
-$megtxt="Denna insider build $env:APPCENTER_BUILD_ID on $env:APPCENTER_BRANCH is now available at: https://install.appcenter.ms/orgs/mahstudio-u5ev/apps/denna/distribution_groups/Insiders"
+$megtxt="Denna insider build $env:APPCENTER_BUILD_ID on $env:APPCENTER_BRANCH is now available at: 
+https://install.appcenter.ms/orgs/mahstudio-u5ev/apps/denna/distribution_groups/Insiders
+
+Any issues? Post is here:
+https://github.com/MahStudio/Denna/issues
+
+or you can join Denna insider chat:
+https://t.me/joinchat/EJ9gLURDAcbOxpoIGSdD8g
+"
 echo $megtxt;
 Invoke-WebRequest -Uri "https://api.telegram.org/bot$env:BotSecret/sendMessage?chat_id=$env:chatId&text=$megtxt"
+Invoke-WebRequest -Uri "https://api.telegram.org/bot$env:BotSecret/sendMessage?chat_id=$env:ChannelId&text=$megtxt"
