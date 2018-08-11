4d07ea68148e88541eb643e572bc90e4fcd72206
Mohsen <mohsens2@outlook.com>
2018-08-11 05:30:39 +0430

    fix #16 Fix #35

--- Denna/Denna/Views/SubSettings/Support.xaml.cs
+++ Denna/Denna/Views/SubSettings/Support.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Denna.Classes;
+using Windows.UI.Xaml.Controls;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -13,5 +14,18 @@ namespace Denna.Views.SubSettings
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e) => "mailto:mohsens22@outlook.com?subject=DENNA_V2_FeedBack".OpenUrl();
+
+        private void Button_Click_1(object sender, Windows.UI.Xaml.RoutedEventArgs e) => "https://install.appcenter.ms/orgs/mahstudio-u5ev/apps/denna/distribution_groups/insiders".OpenUrl();
+
+        private void Button_Click_2(object sender, Windows.UI.Xaml.RoutedEventArgs e) => "https://t.me/joinchat/EJ9gLURDAcbOxpoIGSdD8g".OpenUrl();
+
+
+        private void Button_Click_3(object sender, Windows.UI.Xaml.RoutedEventArgs e) => "https://github.com/MahStudio/Denna/issues".OpenUrl();
+
+        private void Button_Click_4(object sender, Windows.UI.Xaml.RoutedEventArgs e) => "https://github.com/MahStudio/Denna/".OpenUrl();
+
+        private void Button_Click_5(object sender, Windows.UI.Xaml.RoutedEventArgs e) => "http://paypal.me/mohsens22".OpenUrl();
     }
 }
