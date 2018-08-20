8746ed9cf013e83fb1a1da387dc3de377a8f1c60
Mohsen <mohsens2@outlook.com>
2018-08-20 19:17:28 +0430

    fix #17

--- Denna/Denna/Views/SubSettings/Personalization.xaml.cs
+++ Denna/Denna/Views/SubSettings/Personalization.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Core.Utils;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -12,6 +15,51 @@ namespace Denna.Views.SubSettings
         public Personalization()
         {
             InitializeComponent();
+        }
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            try
+            {
+                if (AppSettings.Get<bool>("FollowAccent") == true)
+                    FollowAccent.IsOn = true;
+                else
+                    FollowAccent.IsOn = false;
+            }
+            catch
+            { }
+            try
+            {
+                if (Classes.Themesetter.GetApplicationTheme() == "Dark")
+                    ThemeSelector.SelectedIndex = 0;
+                else if (Classes.Themesetter.GetApplicationTheme() == "Light")
+                    ThemeSelector.SelectedIndex = 1;
+                else
+                    ThemeSelector.SelectedIndex = 2;
+            }
+            catch { }
+            base.OnNavigatedTo(e);
+        }
+        void ThemeSelector_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                Classes.Themesetter.SetApplicationTheme((ThemeSelector.SelectedItem as ComboBoxItem).Tag.ToString());
+            }
+            catch { }
+        }
+
+        void FollowAccent_Toggled(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (FollowAccent.IsOn)
+                    AppSettings.Set("FollowAccent", true);
+                else
+                    AppSettings.Set("FollowAccent", false);
+            }
+            catch
+            {
+            }
         }
     }
 }
