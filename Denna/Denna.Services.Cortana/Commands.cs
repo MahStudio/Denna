00a2ae4e8797deb0c0988b0e3c7012500c078d99
Mohsen <mohsens2@outlook.com>
2018-08-26 00:26:31 +0430

    debug cortana

--- Denna/Denna.Services.Cortana/Commands.cs
+++ Denna/Denna.Services.Cortana/Commands.cs
@@ -105,37 +105,46 @@ namespace Denna.Services.Cortana
 
         async void taskcounter()
         {
-            var x = _service.GetMustDoList().Count;
-            VoiceCommandResponse response = null;
-            if (x == 0)
+            try
             {
-                var userMessage = new VoiceCommandUserMessage();
-                userMessage.DisplayMessage = "No tasks on Denna";
-                userMessage.SpokenMessage = "You have no tasks ! Add one";
-                response =
-                 VoiceCommandResponse.CreateResponse(userMessage);
+                var x = _service.GetMustDoList().Count;
+                VoiceCommandResponse response = null;
+                if (x == 0)
+                {
+                    var userMessage = new VoiceCommandUserMessage();
+                    userMessage.DisplayMessage = "No tasks on Denna";
+                    userMessage.SpokenMessage = "You have no tasks ! Add one";
+                    response =
+                     VoiceCommandResponse.CreateResponse(userMessage);
+                }
+                else
+                {
+                    var userMessage = new VoiceCommandUserMessage();
+                    userMessage.DisplayMessage = "You have " + x + " tasks" + " on Denna";
+                    userMessage.SpokenMessage = "You have " + x + " tasks";
+                    response =
+                     VoiceCommandResponse.CreateResponse(userMessage);
+                }
+                // Create the VoiceCommandResponse from the userMessage and list    
+                // of content tiles.
+
+                // Cortana will present a “Go to app_name” link that the user 
+                // can tap to launch the app. 
+                // Pass in a launch to enable the app to deep link to a page 
+                // relevant to the voice command.
+                response.AppLaunchArgument = "agsonCortana";
+
+                // Ask Cortana to display the user message and content tile and 
+                // also speak the user message.
+                await voiceServiceConnection.ReportSuccessAsync(response);
             }
-            else
+            catch
             {
-                var userMessage = new VoiceCommandUserMessage();
-                userMessage.DisplayMessage = "You have " + x + " tasks" + " on Denna";
-                userMessage.SpokenMessage = "You have " + x + " tasks";
-                response =
-                 VoiceCommandResponse.CreateResponse(userMessage);
+
             }
 
-            // Create the VoiceCommandResponse from the userMessage and list    
-            // of content tiles.
 
-            // Cortana will present a “Go to app_name” link that the user 
-            // can tap to launch the app. 
-            // Pass in a launch to enable the app to deep link to a page 
-            // relevant to the voice command.
-            response.AppLaunchArgument = "agsonCortana";
 
-            // Ask Cortana to display the user message and content tile and 
-            // also speak the user message.
-            await voiceServiceConnection.ReportSuccessAsync(response);
         }
 
 
@@ -143,62 +152,72 @@ namespace Denna.Services.Cortana
 
         async void SendCompletionMessageFortodolist()
         {
-            var destinationsContentTiles = new List<VoiceCommandContentTile>();
-            var mycol = _service.GetTodayList();
-            VoiceCommandResponse response = null;
-            if (mycol.Count == 0)
+            try
             {
-                var userMessage = new VoiceCommandUserMessage();
-                userMessage.DisplayMessage = "Nothing in Denna";
-                userMessage.SpokenMessage = "You have no tasks ! add one.";
-                response = VoiceCommandResponse.CreateResponse(userMessage);
-            }
-            else
-            {
-                var userMessage = new VoiceCommandUserMessage();
-                userMessage.DisplayMessage = "Here's your to do list on Denna";
-                userMessage.SpokenMessage = "Here's your to do list";
-                foreach (var item in mycol)
+                var destinationsContentTiles = new List<VoiceCommandContentTile>();
+                var mycol = _service.GetTodayList();
+                VoiceCommandResponse response = null;
+                if (mycol.Count == 0)
                 {
-                    var destinationTile = new VoiceCommandContentTile();
-                    destinationTile.ContentTileType = VoiceCommandContentTileType.TitleWithText;
-                    destinationTile.AppLaunchArgument = "agsonCortana";
-                    destinationTile.Title = item.Subject;
-                    destinationTile.TextLine1 = item.Detail;
-                    destinationTile.TextLine2 = item.StartTime.ToString();
-                    destinationsContentTiles.Add(destinationTile);
-                    response =
-               VoiceCommandResponse.CreateResponse(
-                   userMessage, destinationsContentTiles);
+                    var userMessage = new VoiceCommandUserMessage();
+                    userMessage.DisplayMessage = "Nothing in Denna";
+                    userMessage.SpokenMessage = "You have no tasks ! add one.";
+                    response = VoiceCommandResponse.CreateResponse(userMessage);
                 }
+                else
+                {
+                    var userMessage = new VoiceCommandUserMessage();
+                    userMessage.DisplayMessage = "Here's your to do list on Denna";
+                    userMessage.SpokenMessage = "Here's your to do list";
+                    foreach (var item in mycol)
+                    {
+                        var destinationTile = new VoiceCommandContentTile();
+                        destinationTile.ContentTileType = VoiceCommandContentTileType.TitleWithText;
+                        destinationTile.AppLaunchArgument = "agsonCortana";
+                        destinationTile.Title = item.Subject;
+                        destinationTile.TextLine1 = item.Detail;
+                        destinationTile.TextLine2 = item.StartTime.ToString();
+                        destinationsContentTiles.Add(destinationTile);
+                        response =
+                   VoiceCommandResponse.CreateResponse(
+                       userMessage, destinationsContentTiles);
+                    }
+                }
+
+                // Create the VoiceCommandResponse from the userMessage and list    
+                // of content tiles.
+
+                // Cortana will present a “Go to app_name” link that the user 
+                // can tap to launch the app. 
+                // Pass in a launch to enable the app to deep link to a page 
+                // relevant to the voice command.
+                response.AppLaunchArgument = "agsonCortana";
+
+                // Ask Cortana to display the user message and content tile and 
+                // also speak the user message.
+                await voiceServiceConnection.ReportSuccessAsync(response);
             }
+            catch { }
 
-            // Create the VoiceCommandResponse from the userMessage and list    
-            // of content tiles.
-
-            // Cortana will present a “Go to app_name” link that the user 
-            // can tap to launch the app. 
-            // Pass in a launch to enable the app to deep link to a page 
-            // relevant to the voice command.
-            response.AppLaunchArgument = "agsonCortana";
-
-            // Ask Cortana to display the user message and content tile and 
-            // also speak the user message.
-            await voiceServiceConnection.ReportSuccessAsync(response);
         }
 
         async void LaunchAppInForeground()
         {
-            var userMessage = new VoiceCommandUserMessage();
-            userMessage.SpokenMessage = "Launching Denna";
+            try
+            {
+                var userMessage = new VoiceCommandUserMessage();
+                userMessage.SpokenMessage = "Launching Denna";
 
-            var response = VoiceCommandResponse.CreateResponse(userMessage);
+                var response = VoiceCommandResponse.CreateResponse(userMessage);
 
-            // When launching the app in the foreground, pass an app 
-            // specific launch parameter to indicate what page to show.
-            response.AppLaunchArgument = "agsonCortana";
+                // When launching the app in the foreground, pass an app 
+                // specific launch parameter to indicate what page to show.
+                response.AppLaunchArgument = "agsonCortana";
 
-            await voiceServiceConnection.RequestAppLaunchAsync(response);
+                await voiceServiceConnection.RequestAppLaunchAsync(response);
+            }
+            catch { }
+
         }
     }
 }
